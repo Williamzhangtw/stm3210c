c5a0804bd817fd3515c1437bc113e98abd3a6945
root <root@DESKTOP-0V9K0CT.localdomain>
2018-05-01 06:32:40 -0700

    generate uvpro

---
+++ MDK-ARM/RTE/_stm3210c/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm3210c' 
+ * Target:  'stm3210c' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
